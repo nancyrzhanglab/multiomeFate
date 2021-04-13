717c5f5a4ae5120fb51bc3323b97e8d340b42e6c
Kevin Lin <klin1234@gmail.com>
2021-04-13 14:39:58 -0400

    Forgot to export plot_umap

--- R/generics.R
+++ R/generics.R
@@ -1,3 +1,10 @@
+#' Plot UMAP embedding (generic)
+#'
+#' @param obj object
+#' @param ... additional parameters
+#'
+#' @return nothing. A plot is made
+#' @export
 plot_umap <- function(obj, ...) {
   UseMethod(generic = "plot_umap", object = obj)
 }
