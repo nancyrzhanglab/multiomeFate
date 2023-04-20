c2ec4335ea1e946f5149f115b7ad3f0a2b3cd546
linnykos <klin1234@gmail.com>
2023-04-20 18:53:16 -0400

    Pushing untested edits to peak modeling

--- R/peak_modeling.R
+++ R/peak_modeling.R
@@ -1,16 +1,16 @@
-peak_mixture_modeling <- function(bin_limits, # vector of length 2
-                                  bin_midpoints, # midpoints of each bin
+peak_mixture_modeling <- function(bandwidth,
                                   cutmat, # rows = cells, columns = basepairs
                                   peak_locations,
                                   peak_prior,
+                                  peak_width,
+                                  discretization_stepsize = bandwidth/5,
                                   bool_lock_within_peak = T, 
                                   bool_freeze_prior = F, # set to T if optimization is done to too few fragments. this is the prior over peaks
                                   max_iter = 100,
                                   min_prior = 0.01,
+                                  num_peak_limit = 4,
                                   return_assignment_mat = F, # set to T usually for only debugging purposes
-                                  return_assignment_intial = F, # set to T usually for only debugging purposes
                                   return_bin_mat = F, # set to T usually for only debugging purposes
-                                  return_theta_initial = F, # set to T usually for only debugging purposes
                                   tol = 1e-6,
                                   verbose = 1){
   stopifnot(length(bin_midpoints) %% 2 == 1,
@@ -18,85 +18,77 @@ peak_mixture_modeling <- function(bin_limits, # vector of length 2
   
   # initial assignment
   num_bins <- length(bin_midpoints)
-  bin_mat <- .compute_bin_matrix(
-    bin_limits = bin_limits,
-    bin_midpoints = bin_midpoints,
+  bin_mat <- .compute_frag_peak_matrix(
     bool_lock_within_peak = bool_lock_within_peak,
     cutmat = cutmat,
-    peak_locations = peak_locations
+    num_peak_limit = num_peak_limit,
+    peak_locations = peak_locations,
+    peak_width = peak_width
   )
   num_frags <- nrow(bin_mat)
- 
-  theta_vec <- .initialize_theta(bin_mat = bin_mat,
-                                 num_bins = num_bins)
-  if(return_theta_initial) theta_init <- theta_vec else theta_init <- NULL
-  if(verbose > 2) print(round(theta_vec, 2))
+  
+  grenander_obj <- .initialize_grenander(bandwidth = bandwidth,
+                                         bin_mat = bin_mat,
+                                         discretization_stepsize = discretization_stepsize)
   
   # start iteration
   if(verbose > 0) print("Starting initialization")
   iter <- 1
   loglikelihood_vec <- .compute_loglikelihood(
     bin_mat = bin_mat,
-    prior_vec = peak_prior,
-    theta_vec = theta_vec
+    grenander_obj = grenander_obj,
+    prior_vec = peak_prior
   )
   if(verbose > 1) print(paste0("Initial log-likelihood: ", round(loglikelihood_vec[1],2)))
-  theta_diff <- NA
   prior_vec <- peak_prior
+  
+  # TODO: Return if there are no fragments
+  
   while(TRUE){
     if(iter > max_iter) break()
     
     if(verbose > 1) print("E-step")
     assignment_mat <- .e_step(
       bin_mat = bin_mat,
-      prior_vec = prior_vec,
-      theta_vec = theta_vec
+      grenander_obj = grenander_obj,
+      prior_vec = prior_vec
     )
-    if(iter == 1){
-      if(return_assignment_intial) assignment_init <- assignment_mat else assignment_init <- NULL
-    }
     
     if(verbose > 1) print("M-step")
-    theta_vec_new <- .m_step(
+    grenander_obj_new <- .m_step(
       assignment_mat = assignment_mat,
       bin_mat = bin_mat,
       num_bins = num_bins
     )
     if(!bool_freeze_prior) { prior_vec <- .compute_prior(assignment_mat = assignment_mat, min_prior = min_prior) }
-    if(verbose > 2) print(round(theta_vec_new, 2))
-    
+   
     if(verbose) print("Computing likelihood")
     loglikelihood_val <- .compute_loglikelihood(
       bin_mat = bin_mat,
-      prior_vec = prior_vec,
-      theta_vec = theta_vec_new
+      grenander_obj = grenander_obj,
+      prior_vec = peak_prior
     )
     
     loglikelihood_vec <- c(loglikelihood_vec, loglikelihood_val)
-    theta_diff <- c(theta_diff, sum(abs(theta_vec_new - theta_vec)))
     iter <- length(loglikelihood_vec)
     if(length(loglikelihood_vec) >= 2){
       if(verbose > 0) print(paste0("Iteration: ", iter, ", log-likelihood: ", round(loglikelihood_vec[iter],2)))
-      if(abs(loglikelihood_vec[iter] - loglikelihood_vec[iter-1]) <= tol &
-         theta_diff[iter] <= tol) break()
+      if(abs(loglikelihood_vec[iter] - loglikelihood_vec[iter-1]) <= tol) break()
     }
-    theta_vec <- theta_vec_new
+    grenander_obj <- grenander_obj_new
   }
   
   if(!return_bin_mat) bin_mat <- NULL
   if(!return_assignment_mat) assignment_mat <- NULL
- 
-  structure(list(assignment_init = assignment_init,
-                 assignment_mat = assignment_mat,
+  
+  structure(list(assignment_mat = assignment_mat,
                  bin_mat = bin_mat,
+                 grenander_obj = grenander_obj,
                  iter = length(loglikelihood_vec),
                  loglikelihood_val = loglikelihood_vec[length(loglikelihood_vec)],
                  loglikelihood_vec = loglikelihood_vec,
                  num_frags = num_frags,
-                 prior_vec = prior_vec,
-                 theta_diff = theta_diff,
-                 theta_init = theta_init,
-                 theta_vec = theta_vec),
+                 prior_vec = prior_vec),
             class = "peakDistribution")
 }
 
@@ -150,19 +142,16 @@ compute_peak_prior <- function(mat,
 
 ##################
 
-.compute_bin_matrix <- function(bin_limits,
-                                bin_midpoints,
-                                bool_lock_within_peak,
-                                cutmat,
-                                peak_locations){
-  stopifnot(all(bin_midpoints >= bin_limits[1]), 
-            all(bin_midpoints <= bin_limits[2]))
+# compute a frag-by-peak matrix that denotes the distance from each frag to each peak, and NA otherwise
+.compute_frag_peak_matrix <- function(bool_lock_within_peak,
+                                      cutmat,
+                                      num_peak_limit,
+                                      peak_locations,
+                                      peak_width){
   # a matrix with nrow = number of fragments, and ncol = number of peaks
   n <- nrow(cutmat)
   p <- length(peak_locations)
-  cutmat_t <- Matrix::t(cutmat)
-  num_bins <- length(bin_midpoints)
-  bin_index <- (-(num_bins-1)/2):((num_bins-1)/2)
+  cutmat_t <- Matrix::t(cutmat) # basepair-by-cell
   
   # a list, where each entry is a matrix of "fragments by peaks" where its entry is which distance-bin it is
   cell_list <- lapply(1:n, function(i){
@@ -173,31 +162,12 @@ compute_peak_prior <- function(mat,
     fragment_locations <- as.numeric(colnames(cutmat))[fragment_idx]
     frag_len <- length(fragment_locations)
     
-    # for each fragment, return a vector (length of peaks) of which distance-bin is used
+    # for each fragment, return a vector (length of peaks) of the distance to the peak
     tmp <- sapply(fragment_locations, function(j){
-      distance_vec <- peak_locations - j
-      # two settings: one if any least one peak is within bin_limits
-      if(length(intersect(which(distance_vec >= bin_limits[1]), which(distance_vec <= bin_limits[2])))){
-        tmp2 <- sapply(distance_vec, function(dist){
-          min_val <- min(abs(dist - bin_midpoints))
-          if(min_val >= bin_limits[1] & (min_val <= bin_limits[2])){
-            bin_index[which.min(abs(dist - bin_midpoints))]
-          } else {
-            NA
-          }
-        })
-      } else {
-        # the other if no peaks is within bin_limits
-        tmp2 <- rep(NA, p)
-        idx <- which.min(abs(distance_vec))
-        tmp2[idx] <- bin_index[which.min(abs(distance_vec[idx] - bin_midpoints))]
-      }
-      
-      tmp2
+      peak_locations - j
     })
     
-    if(!is.matrix(tmp)) tmp <- matrix(tmp, nrow = length(tmp), ncol = p)
-    if(all(dim(tmp) == c(p,frag_len))) tmp <- t(tmp)
+    if(!is.matrix(tmp)) tmp <- matrix(tmp, nrow = frag_len, ncol = p)
     stopifnot(all(dim(tmp) == c(frag_len,p)))
     rownames(tmp) <- paste0("c:", i, "_loc:", fragment_locations)
     tmp
@@ -210,7 +180,7 @@ compute_peak_prior <- function(mat,
   # apply bool_lock_within_peak
   if(bool_lock_within_peak) {
     for(i in 1:nrow(res)){
-      idx <- which(res[i,] == 0)
+      idx <- which(abs(res[i,]) <= peak_width/2)
       if(length(idx) > 0){
         if(length(idx) > 1) idx <- sample(idx, 1)
         res[i,-idx] <- NA
@@ -218,76 +188,94 @@ compute_peak_prior <- function(mat,
     }
   }
   
-  res
+  # apply num_peak_limit
+  if(!is.na(num_peak_limit)){
+    for(i in 1:nrow(res)){
+      # fragments to the left of the peak
+      idx <- which(res[i,] >= 0)
+      if(length(idx) > num_peak_limit) {
+        dist_vec <- abs(res[i,idx])
+        cutoff_dist <- sort(dist_vec, decreasing = F)[num_peak_limit]
+        rm_idx <- idx[which(dist_vec > cutoff_dist)]
+        res[i,rm_idx] <- NA
+      }
+      
+      # fragments to the right of the peak
+      idx <- which(res[i,] <= 0)
+      if(length(idx) > num_peak_limit) {
+        dist_vec <- abs(res[i,idx])
+        cutoff_dist <- sort(dist_vec, decreasing = F)[num_peak_limit]
+        rm_idx <- idx[which(dist_vec > cutoff_dist)]
+        res[i,rm_idx] <- NA
+      }
+    }
+  }
+  
+  # convert all signed distances into unsigned distances
+  abs(res)
 }
 
-.initialize_theta <- function(bin_mat,
-                              num_bins){
-  min_vec <- sapply(1:nrow(bin_mat), function(i){
-    vec <- bin_mat[i,]
-    if(all(is.na(vec))) return(NA)
-    
-    avec <- abs(vec)
-    min_val <- min(avec, na.rm = T)
-    idx <- which(avec == min_val)
-    if(length(idx) == 1) {
-      return(vec[idx])
-    } else {
-      return(vec[sample(idx,1)])
-    }
-  })
+.initialize_grenander <- function(bandwidth,
+                                  bin_mat,
+                                  discretization_stepsize){
+  idx <- which(!is.na(bin_mat))
+  values <- as.numeric(bin_mat[idx])
+  weights <- rep(1, length(values))
   
-  bin_index <- (-(num_bins-1)/2):((num_bins-1)/2)
-  theta_vec <- sapply(bin_index, function(i){
-    length(which(min_vec == i))
-  })
-  theta_vec <- theta_vec/sum(theta_vec)
-  names(theta_vec) <- paste0("bin:", bin_index)
-  theta_vec
+  estimate_grenander(values = values,
+                     weights = weights,
+                     bandwidth = bandwidth,
+                     discretization_stepsize = discretization_stepsize)
 }
 
 .compute_loglikelihood <- function(bin_mat,
+                                   grenander_obj,
                                    prior_vec,
-                                   theta_vec,
                                    tol = 1e-6){
   m <- nrow(bin_mat) # number of fragments
   p <- ncol(bin_mat) # number of peaks
-  stopifnot(length(theta_vec) >= max(bin_mat, na.rm = T),
-            abs(sum(prior_vec) - 1) <= tol)
+  stopifnot(abs(sum(prior_vec) - 1) <= tol)
   
-  # handling NAs: fragment-peak pairs that are too far
-  mask_idx <- which(is.na(bin_mat))
-  bin_mat[mask_idx] <- 0
+  # compute all the densities based on grenander
+  idx <- which(!is.na(bin_mat))
+  if(length(idx) == 0 || m == 0) return(NA) # if no fragments
+  for(i in 1:idx){
+    bin_mat[i] <- evaluate_grenander(
+      obj = grenander_obj,
+      x = bin_mat[i]
+    )
+  }
+  if(length(idx) != prod(dim(bin_mat))) {
+    bin_mat[-idx] <- 0
+  }
   
-  num_bins <- length(theta_vec)
-  reidx <- (num_bins-1)/2+1
-  # remember values in bin_mat are from -(num_bins-1)/2 to (num_bins-1)/2, so we need to re-index them
-  tmp <- matrix(theta_vec[bin_mat + reidx], nrow = m, ncol = p)
-  tmp[mask_idx] <- 0
-  tmp <- tmp %*% diag(prior_vec)
+  tmp <- bin_mat %*% diag(prior_vec)
   sum_vec <- Matrix::rowSums(tmp)
   sum(log(sum_vec[sum_vec >= tol]))
 }
 
 # compute assignment_mat
 .e_step <- function(bin_mat,
+                    grenander_obj,
                     prior_vec,
-                    theta_vec,
                     tol = 1e-6){
   m <- nrow(bin_mat) # number of fragments
   p <- ncol(bin_mat) # number of peaks
-  stopifnot(length(prior_vec) == p)
+  stopifnot(abs(sum(prior_vec) - 1) <= tol, m > 0, p > 0)
   
-  # handling NAs: fragment-peak pairs that are too far
-  mask_idx <- which(is.na(bin_mat))
-  bin_mat[mask_idx] <- 0
+  # compute all the densities based on grenander
+  idx <- which(!is.na(bin_mat))
+  for(i in 1:idx){
+    bin_mat[i] <- evaluate_grenander(
+      obj = grenander_obj,
+      x = bin_mat[i]
+    )
+  }
+  if(length(idx) != prod(dim(bin_mat))) {
+    bin_mat[-idx] <- 0
+  }
   
-  num_bins <- length(theta_vec)
-  reidx <- (num_bins-1)/2+1
-  # remember values in bin_mat are from -(num_bins-1)/2 to (num_bins-1)/2, so we need to re-index them
-  tmp <- matrix(theta_vec[bin_mat + reidx], nrow = m, ncol = p)
-  tmp[mask_idx] <- 0
-  tmp <- tmp %*% diag(prior_vec)
+  tmp <- bin_mat %*% diag(prior_vec)
   sum_vec <- rowSums(tmp)
   idx <- which(sum_vec <= tol)
   if(length(idx) > 0) sum_vec[sum_vec <= tol] <- 1
@@ -300,20 +288,19 @@ compute_peak_prior <- function(mat,
 }
 
 .m_step <- function(assignment_mat,
+                    bandwidth,
                     bin_mat,
-                    num_bins){
-  stopifnot(all(dim(assignment_mat) == dim(bin_mat)))
+                    discretization_stepsize){
+  idx <- which(!is.na(bin_mat))
+  stopifnot(all(dim(assignment_mat) == dim(bin_mat)), length(idx) > 0)
   
-  bin_index <- (-(num_bins-1)/2):((num_bins-1)/2)
-  beta_vec <- sapply(bin_index, function(i){
-    # find all the peaks that are the appropriate distance
-    idx <- which(bin_mat == i)
-    sum(assignment_mat[idx])
-  })
+  values <- as.numeric(bin_mat[idx])
+  weights <- as.numeric(assignment_mat[idx])
   
-  beta_vec <- beta_vec/sum(beta_vec)
-  names(beta_vec) <- paste0("bin:", bin_index)
-  beta_vec
+  estimate_grenander(values = values,
+                     weights = weights,
+                     bandwidth = bandwidth,
+                     discretization_stepsize = discretization_stepsize)
 }
 
 .compute_prior <- function(assignment_mat,
