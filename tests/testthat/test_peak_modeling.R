76a8bf278dc9e2c3c15eab922c89c25ef690454a
linnykos <klin1234@gmail.com>
2023-04-24 15:08:29 -0400

    Found a simple example of a failing case of non-increasing lowerbound

--- tests/testthat/test_peak_modeling.R
+++ tests/testthat/test_peak_modeling.R
@@ -258,8 +258,38 @@ test_that(".m_step works", {
 
 ## peak_mixture_modeling is correct
 
-# load("tests/assets/test.RData")
 test_that("peak_mixture_modeling works", {
+  # hypothetical genome from base-pairs 1001 to 2000
+  # 120 fragments, one per cell
+  # 2 peaks (at 1500 and at 1900), most fragments at 1300, 1800 or 1100
+  set.seed(10)
+  peak_locations <- c(1500, 1900)
+  names(peak_locations) <- paste0("p:", 1:2)
+  num_frags <- 120
+  frag_location <- rep(c(1100,1300,1800), each = num_frags/3)
+  cutmat <- sapply(frag_location, function(x){
+    vec <- rep(0, length = 1000)
+    names(vec) <- c(1001:2000)
+    vec[as.character(x)] <- 1
+    vec
+  })
+  cutmat <- Matrix::Matrix(t(cutmat), sparse = T)
+  rownames(cutmat) <- paste0("cell:", 1:nrow(cutmat))
+  peak_prior <- c(0.5, 0.5)
+  peak_width <- 250
+  
+  res <- peak_mixture_modeling(cutmat = cutmat,
+                               peak_locations = peak_locations,
+                               peak_prior = peak_prior,
+                               peak_width = peak_width,
+                               bool_freeze_prior = F,
+                               return_lowerbound = T,
+                               verbose = 0)
+  
+})
+
+# load("tests/assets/test.RData")
+test_that("peak_mixture_modeling works on a real cutmat", {
   load("../assets/test.RData")
   res1 <- peak_mixture_modeling(cutmat = cutmat_dying,
                                 peak_locations = peak_locations,
