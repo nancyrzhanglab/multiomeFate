87a417f4a3b0af5bb5d1eded769d2dd1eb2ce10f
linnykos <klin1234@gmail.com>
2023-04-25 12:44:22 -0400

    Pushing minor changes

--- R/peak_modeling.R
+++ R/peak_modeling.R
@@ -119,42 +119,6 @@ peak_mixture_modeling <- function(cutmat, # rows = cells, columns = basepairs
             class = "peakDistribution")
 }
 
-compute_peak_locations <- function(peak_mat){
-  res <- sapply(1:nrow(peak_mat), function(i){
-    round(mean(peak_mat[i,]))
-  })
-  names(res) <- paste0("p:", 1:nrow(peak_mat))
-  res
-}
-
-compute_peak_prior <- function(mat,
-                               peak_mat,
-                               min_prior = 0.01){
-  peak_bp <- as.numeric(colnames(mat))
-  count_vec <- sapply(1:nrow(peak_mat), function(i){
-    idx <- intersect(which(peak_bp >= peak_mat[i,"start"]),
-                     which(peak_bp <= peak_mat[i,"end"]))
-    sum(sapply(idx, function(j){
-      length(.nonzero_col(mat = mat,
-                          col_idx = j,
-                          bool_value = F))
-    }))
-  })
-  
-  prior_vec <- count_vec/sum(count_vec)
-  if(any(prior_vec <= min_prior)){
-    if(min_prior*length(prior_vec) > 1) {
-      min_prior <- 1/(2*length(prior_vec))
-    }
-    prior_vec <- prior_vec + min_prior/(1-min_prior*length(prior_vec))
-    prior_vec <- prior_vec/sum(prior_vec)
-  }
-  names(prior_vec) <- paste0("p:", 1:nrow(peak_mat))
-  
-  stopifnot(all(prior_vec >= 0))
-  prior_vec
-}
-
 ##################
 
 # compute a frag-by-peak matrix that denotes the distance from each frag to each peak, and NA otherwise
---
+++ R/peak_util.R
@@ -0,0 +1,35 @@
+compute_peak_locations <- function(peak_mat){
+  res <- sapply(1:nrow(peak_mat), function(i){
+    round(mean(peak_mat[i,]))
+  })
+  names(res) <- paste0("p:", 1:nrow(peak_mat))
+  res
+}
+
+compute_peak_prior <- function(cutmat,
+                               peak_mat,
+                               min_prior = 0){
+  peak_bp <- as.numeric(colnames(cutmat))
+  count_vec <- sapply(1:nrow(peak_mat), function(i){
+    idx <- intersect(which(peak_bp >= peak_mat[i,"start"]),
+                     which(peak_bp <= peak_mat[i,"end"]))
+    sum(sapply(idx, function(j){
+      length(.nonzero_col(mat = mat,
+                          col_idx = j,
+                          bool_value = F))
+    }))
+  })
+  
+  prior_vec <- count_vec/sum(count_vec)
+  if(any(prior_vec <= min_prior)){
+    if(min_prior*length(prior_vec) > 1) {
+      min_prior <- 1/(2*length(prior_vec))
+    }
+    prior_vec <- prior_vec + min_prior/(1-min_prior*length(prior_vec))
+    prior_vec <- prior_vec/sum(prior_vec)
+  }
+  names(prior_vec) <- paste0("p:", 1:nrow(peak_mat))
+  
+  stopifnot(all(prior_vec >= 0))
+  prior_vec
+}
