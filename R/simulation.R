cf0f11da9f9c4adb3a7982290db65e5bb33c6e1c
linnykos <kzlin@uw.edu>
2024-04-17 10:30:14 -0700

    Ooops

--- R/simulation.R
+++ R/simulation.R
@@ -89,7 +89,164 @@ generate_simulation<- function(embedding_mat,
     print("Step 6: Outputting")
   list(cell_fate_potential = log10(cell_contribution_random + 1), 
        cell_fate_potential_truth = log10(cell_contribution_truth), 
-       coefficient_intercept = coefficient_intercept, coefficient_vec = coefficient_vec, 
-       gaussian_list = gaussian_list, lineage_assignment = lineage_assignment, 
-       lineage_future_size = lineage_future_size, prob_mat = prob_mat)
+       coefficient_intercept = coefficient_intercept, 
+       coefficient_vec = coefficient_vec, 
+       gaussian_list = gaussian_list, 
+       lineage_assignment = lineage_assignment, 
+       lineage_future_size = lineage_future_size, 
+       prob_mat = prob_mat)
+}
+
+
+#############
+
+.kmeans_seed <- function(
+    embedding_mat,
+    K
+){
+  kmeans_res <- suppressWarnings(stats::kmeans(embedding_mat, centers = 2*K))
+  cluster_idxs <- sapply(1:K, function(k){
+    sample(which(kmeans_res$cluster == k), 1)
+  })
+  names(cluster_idxs) <- paste0("lineage:", 1:K)
+  cluster_idxs
+}
+
+.gaussian <- function(cov_mat,
+                      mean_vec){
+  stopifnot(nrow(cov_mat) == ncol(cov_mat),
+            nrow(cov_mat) == length(mean_vec))
+  
+  structure(list(cov = cov_mat, 
+                 mean = mean_vec),
+            class = "gaussian")
+}
+
+.form_gaussian_distribution <- function(
+    cluster_idx,
+    embedding_mat,
+    rho
+){
+  stopifnot(length(cluster_idx) == 1,
+            cluster_idx <= nrow(embedding_mat),
+            cluster_idx > 0,
+            cluster_idx %% 1 == 0)
+  
+  mean_vec <- embedding_mat[cluster_idx,]
+  sd_vec <- apply(embedding_mat, 2, stats::sd)
+  
+  cov_mat <- diag(rho*sd_vec^2)
+  
+  .gaussian(cov_mat = cov_mat, 
+            mean_vec = mean_vec)
+}
+
+.form_gaussian_distributions <- function(
+    cluster_idxs,
+    embedding_mat,
+    rho
+){
+  K <- length(cluster_idxs)
+  gaussian_list <- lapply(1:K, function(k){
+    .form_gaussian_distribution(
+      cluster_idx = cluster_idxs[k],
+      embedding_mat = embedding_mat,
+      rho = rho
+    )
+  })
+  names(gaussian_list) <- paste0("lineage:", 1:K)
+  
+  gaussian_list
+}
+
+.compute_posteriors <- function(
+    embedding_mat,
+    gaussian_list,
+    lineage_prior,
+    verbose = 0
+){
+  n <- nrow(embedding_mat)
+  K <- length(lineage_prior)
+  
+  # all the calculations are done on the log scale
+  # we use the log-sum-exp trick: https://gregorygundersen.com/blog/2020/02/09/log-sum-exp/
+  prob_mat <- matrix(NA, nrow = n, ncol = K)
+  if(length(rownames(embedding_mat)) > 0) 
+    rownames(prob_mat) <- rownames(embedding_mat)
+  colnames(prob_mat) <- names(lineage_prior)
+  
+  for(i in 1:n){
+    if(verbose > 0 && n > 10 && i %% floor(n/10) == 0) cat('*')
+    d_vec <- sapply(1:K, function(k){
+      .dmvnorm(x = embedding_mat[i,],
+               mean = gaussian_list[[k]]$mean, 
+               sigma = gaussian_list[[k]]$cov, 
+               log = TRUE, 
+               checkSymmetry = FALSE)
+    })
+    stopifnot(length(lineage_prior) == length(d_vec))
+    log_vec <- log(lineage_prior) + d_vec
+    
+    prob_mat[i,] <- .log_sum_exp_normalization(log_vec)
+  }
+  
+  prob_mat
+}
+
+# we use the log-sum-exp trick: https://gregorygundersen.com/blog/2020/02/09/log-sum-exp/
+# vec is the vector of log(un-normalized probabilities), 
+# and we wish to output the probabilities (non-negative, sums to 1) 
+.log_sum_exp_normalization <- function(x, tol = 1e-6){
+  c <- max(x)
+  y <- c + log(sum(exp(x-c)))
+  res <- exp(x-c)
+  res <- res/sum(res)
+  
+  stopifnot(all(res >= -tol, abs(sum(res)-1) <= tol))
+  
+  res
+}
+
+# from the mvtnorm package: https://github.com/cran/mvtnorm/blob/master/R/mvnorm.R
+.dmvnorm <- function (x, 
+                      mean = rep(0, p), 
+                      sigma = diag(p), 
+                      log = FALSE, 
+                      checkSymmetry = TRUE)
+{
+  
+  if (is.vector(x))
+    x <- matrix(x, ncol = length(x))
+  p <- ncol(x)
+  
+  if(!missing(mean)) {
+    if(!is.null(dim(mean))) dim(mean) <- NULL
+    if (length(mean) != p)
+      stop("x and mean have non-conforming size")
+  }
+  if(!missing(sigma)) {
+    if (p != ncol(sigma))
+      stop("x and sigma have non-conforming size")
+    if (checkSymmetry && !Matrix::isSymmetric(
+      sigma, 
+      tol = sqrt(.Machine$double.eps), 
+      check.attributes = FALSE))
+      stop("sigma must be a symmetric matrix")
+  }
+  
+  ## <faster code contributed by Matteo Fasiolo mf364 at bath.ac.uk
+  dec <- tryCatch(base::chol(sigma), error=function(e)e)
+  if (inherits(dec, "error")) {
+    ## warning("cannot compute chol(sigma)"); return(NaN)
+    ## behave the same as dnorm(): return Inf or 0
+    x.is.mu <- colSums(t(x) != mean) == 0
+    logretval <- rep.int(-Inf, nrow(x))
+    logretval[x.is.mu] <- Inf # and all other f(.) == 0
+  } else {
+    tmp <- backsolve(dec, t(x) - mean, transpose = TRUE)
+    rss <- colSums(tmp ^ 2)
+    logretval <- - sum(log(diag(dec))) - 0.5 * p * log(2 * pi) - 0.5 * rss
+  }
+  names(logretval) <- rownames(x)
+  if(log) logretval else exp(logretval)
 }
